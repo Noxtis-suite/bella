b098d6745d218ea02c6b43eb4d058a9a371609e2
noxtis <frederikwer@gmail.com>
2025-05-22 15:48:19 +0200

    new database

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: [
       // Allow images from the Supabase storage
-      "aylbdzyhadioauxeptum.supabase.co",
+      "supabasekong-swogg0csoo4kwk8wk800woog.138.201.120.43.sslip.io",
     ],
   },
 };
